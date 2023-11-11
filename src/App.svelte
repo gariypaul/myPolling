81320bf728868bfc450158e4cd28b2b97ebb25b6
Paul Odhiambo Gariy <Paul.O.Gariy-1@ou.edu>
2023-11-10 21:38:11 -0600

    Initialised application

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+	import Header from './components/Header.svelte';
+	import Footer from './components/Footer.svelte';
+
+	let items = ["Current Polls", "Add New Poll"];
+	let activeItem = "Current Polls";
+
+</script>
+
+
+<Header/>
+<main>
+	<h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat interdum varius sit amet mattis vulputate enim nulla aliquet</h1>
+</main>
+<Footer/>
+
+
+
+
+<style>
+	main{
+		max-width:960px;
+		margin: 40px auto;
+	}
+</style>
+
---
+++ src/components/Footer.svelte
@@ -0,0 +1,18 @@
+<footer>
+    <div class="copyright">Copyright 2023 Paul Gariy</div>
+</footer>
+
+<style>
+    footer{
+
+        padding: 40px;
+        text-align: center;
+    }    
+    .copyright{
+        color:#aaaa;
+        font-size: 14px;
+        display: inline-block;
+        padding: 20px;
+        border-top: 1px solid #ddd;
+    }
+</style>
