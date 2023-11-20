0fd5d0b5d4d58a909d334bd8dc3f490bf1e3151f
Paul Odhiambo Gariy <Paul.O.Gariy-1@ou.edu>
2023-11-20 12:39:53 -0600

    Modifications

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
 	import Header from './components/Header.svelte';
 	import Footer from './components/Footer.svelte';
 	import Tabs from './shared/Tabs.svelte';
+	import CreatePollForm from './components/CreatePollForm.svelte';
 
 	let items = ["Current Polls", "Add New Poll"];
 	let activeItem = "Current Polls";
@@ -19,7 +20,7 @@
 	{#if activeItem==='Current Polls'}
 		<p>Poll List Component Goes here</p>
 	{:else if activeItem==='Add New Poll'}
-		<p>New Poll Form Goes Here</p>
+		<CreatePollForm/>
 	{/if}
 </main>
 <Footer/>
---
+++ src/components/CreatePollForm.svelte
@@ -0,0 +1,22 @@
+<script>
+
+</script>
+
+<form action="">
+    <div class="form-field">
+        <label for="Question">Poll Question</label>
+        <input type="text" id="question">
+    </div>
+    <div class="form-field">
+        <label for="Question">Poll Question</label>
+        <input type="text" id="question">
+    </div>
+    <div class="form-field">
+        <label for="Question">Poll Question</label>
+        <input type="text" id="question">
+    </div>
+</form>
+
+<style>
+
+</style>
