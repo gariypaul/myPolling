b4b34b5867c62d2cc5f6008e0bdf6ab56d0dfeeb
Paul Odhiambo Gariy <Paul.O.Gariy-1@ou.edu>
2023-11-23 22:21:22 -0500

    Created poll detail cards

---
+++ src/shared/Card.svelte
@@ -0,0 +1,12 @@
+<div class="card">
+    <slot></slot>
+</div>
+
+,<style>
+    .card{
+        background: #fff;
+        padding: 20px;
+        border-radius: 6px;
+        box-shadow: 0px 3px 5px rgba(0,0,0,0.1);
+    }
+</style>
